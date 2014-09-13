4bafb31b215fbfb6f9e2ed7085616fe9f51b7dc5
Andrew Smith <andrewsmith@alumni.stanford.edu>
2014-09-13 16:40:37 -0700

    chat app finished!

--- Chatty/chat_app.js
+++ Chatty/chat_app.js
@@ -1,9 +1,30 @@
 var app = require('express')();
 var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
 app.get('/', function(req, res) {
 	res.sendFile(__dirname + '/index.html');
 });
+
+io.on('connection', function (socket) {
+	console.log('a user connected');
+	socket.on('join', function (userName) {
+		socket.nickname = userName;
+		socket.broadcast.emit('userConnect', userName);	
+	});
+	socket.on('disconnect', function () {
+		console.log('user disconnected');
+		socket.broadcast.emit('userDisconnect', socket.nickname);
+	});
+	socket.on('messageFromClient', function (msg) {
+		socket.broadcast.emit('messageFromServer', 
+		{
+			userName: socket.nickname,
+			msg: msg
+		});
+	});
+});
+
 
 http.listen(8080, function () {
 	console.log('listening on *:8080');
